0c8eee5fb184bbfc3abde1e6590abcac07a9ddf0
Polidowze <167921430+Polidowze@users.noreply.github.com>
2024-05-03 21:19:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+int numero;
+
+void loop(){
+Console.WriteLine("__________Como saber se o numero é negativo ou possitivo__________");
+
+Console.WriteLine("Digite seu numero:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if(numero < 0){
+
+Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("este numero não existe no banco de dados pois não é negativo.");
+
+}
+
+if(numero > 0){
+
+Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Seu numero é positivo.");
+
+}
+
+Console.ResetColor();
+
+ Console.WriteLine("Você quer recomeçar o programa? [S/N]");
+    string reposta = Console.ReadLine()!;
+    if(reposta.ToUpper() == "S")
+    {
+    Console.ResetColor();
+        loop();
+    }
+    else{
+
+    }
+}
+loop();
